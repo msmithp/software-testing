c403cf5307fe0f18441207f6df07fc49d5dfa678
msmithp <matthewsmith2003@yahoo.com>
2025-04-01 12:41:17 -0400

    Update description of equals_float()

--- activities/activity3/test_triangle.py
+++ activities/activity3/test_triangle.py
@@ -3,8 +3,7 @@ import pytest
 import math
 
 def equals_float(a, b, epsilon=0.05):
-    """ Determine if two floating-point values are 
-        equal to two decimal places """
+    """ Determine if two floating-point values are roughly equal """
     return abs(a - b) <= epsilon 
 
 # Write your tests here
